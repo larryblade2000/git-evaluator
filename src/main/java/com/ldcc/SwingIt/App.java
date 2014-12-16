55d867399be3c3a8730d9b4a558de6846618420c
Larry Blade <larryblade@yahoo.com>
2014-12-16 16:10:39 +0100

    added Swing@

--- src/main/java/com/ldcc/SwingIt/App.java
+++ src/main/java/com/ldcc/SwingIt/App.java
@@ -1,13 +1,35 @@
 package com.ldcc.SwingIt;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+  
+  private static void createAndShowGUI() {
+    //Create and set up the window.
+    JFrame frame = new JFrame("HelloWorldSwing");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Add the ubiquitous "Hello World" label.
+    JLabel label = new JLabel("Hello World");
+    frame.getContentPane().add(label);
+
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+          public void run() {
+            createAndShowGUI();
+          }
+        });
+
     }
 }
